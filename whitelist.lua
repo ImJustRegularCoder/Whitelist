541e62bfbae5514b3db44d250e66339161736b26
ImJustRegularCoder <143939562+ImJustRegularCoder@users.noreply.github.com>
2023-09-13 15:15:05 +0200

    cool message isnt it

--- whitelist.lua
+++ whitelist.lua
@@ -1,10 +1,7 @@
---\\ This is open source, btw. You're not impressing anyone by leaking it.
---\\ Want to know what this is used for? It's the Vyon premium whitelist. Get more info on our Discord.
-
-
 local VyonPremium = {
     3821489932,
     4463110410,
     2226003580,
+    2,
 }
 return VyonPremium
